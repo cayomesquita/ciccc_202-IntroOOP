89233b82bc63dca93b1fe7049049452ca102c182
Cayo <cayomesquita@gmail.com>
2020-02-10 10:24:01 -0800

    Initialize project

---
+++ src/basics/HelloWorld.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("Hello, world!");
+        System.out.println(String.format("%s starts at %02d:%02d", "Java", 8, 6));
+    }
+}

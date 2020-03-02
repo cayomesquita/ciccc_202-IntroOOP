1c8e0db079f3b50e2cef47bb63d4a987975ab340
Cayo <cayomesquita@gmail.com>
2020-03-02 11:02:33 -0800

    Add Lab3

--- src/labs/lab3/Piece.java
+++ src/labs/lab3/Piece.java
@@ -1,4 +1,46 @@
 package labs.lab3;
 
-public class Piece {
+public abstract class Piece {
+
+    protected int value;
+    protected boolean isWhite;
+
+    public Piece(int value, boolean isWhite) {
+        this.value = value;
+        this.isWhite = isWhite;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public boolean isWhite() {
+        return isWhite;
+    }
+
+    public abstract void move();
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj instanceof Piece) {
+            Piece other = (Piece) obj;
+            return this.isWhite == other.isWhite && this.getValue() == other.getValue();
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "value=" + value +
+                ", isWhite=" + isWhite +
+                '}';
+    }
 }
--- src/labs/lab3/Queen.java
+++ src/labs/lab3/Queen.java
@@ -1,4 +1,23 @@
 package labs.lab3;
 
-public class Queen {
+public class Queen extends Piece {
+
+    public static final int DEFAULT_VALUE = 9;
+
+    public Queen(boolean isWhite) {
+        super(DEFAULT_VALUE, isWhite);
+    }
+
+    @Override
+    public void move() {
+        System.out.println("Like bishop and rook");
+    }
+
+    @Override
+    public String toString() {
+        return "Queen{" +
+                "value=" + value +
+                ", isWhite=" + isWhite +
+                '}';
+    }
 }
